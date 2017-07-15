7bc83545f3cb6aec70da904e5c6731d189762b6a
Woyten <woyten.tielesch@online.de>
2017-07-15 21:51:40 +0200

    Binary-Name gemäß Konvention

--- profile.sh
+++ profile.sh
@@ -8,7 +8,7 @@ echo "=== Compiling with C ==="
 g++ -O3 *.c
 
 echo "=== Running with Rust ==="
-time target/release/FDTDS -n out-rust -o bin3d -g 200
+time target/release/fdtds -n out-rust -o bin3d -g 200
 
 echo "=== Running with C ==="
 time ./a.out -n out-c -o bin3d -g 200
